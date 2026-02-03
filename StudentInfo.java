66075811fcd211f934cc6d96dcba2342dc84f2eb
Joseph Abel <josephabel540@gmail.com>
2026-02-03 23:46:42 +0100

    java

---
+++ StudentInfo.java
@@ -0,0 +1,17 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String name = "Joseph Abel Olayinka";
+        String matricNumber = "202590006199GA";
+        String faculty = "Computing";
+        String department = "Computer Science";
+        String level = "200 Level";
+        
+        System.out.println("========== STUDENT INFORMATION ==========");
+        System.out.println("Name: " + name);
+        System.out.println("Matric Number: " + matricNumber);
+        System.out.println("Faculty: " + faculty);
+        System.out.println("Department: " + department);
+        System.out.println("Level: " + level);
+        System.out.println("=========================================");
+    }
+}
