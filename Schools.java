fcb452281a0e9884eb1b1d1b1579004095345e63
Joseph Abel <josephabel540@gmail.com>
2026-03-02 23:46:32 +0100

    edit

--- Schools.java
+++ Schools.java
@@ -1,6 +1,7 @@
 public class Schools {
     public String principal;
     private String accountNo;
+    public String bankName;
     private String phoneNo;
 
     public String getPrincipal() {
@@ -19,6 +20,13 @@ public class Schools {
         this.accountNo = accountNo;
     }
 
+    public String getBankName(){
+        return bankName;
+    }
+    public void setBankName(String bankName){
+        this.bankName= bankName;
+    }
+
     public String getPhoneNo() {
         return phoneNo;
     }
@@ -31,10 +39,12 @@ public class Schools {
         Schools s = new Schools();
         s.setPrincipal("Mr. Abel");
         s.setAccountNo("2096930863");
+        s.setBankName("united bank of africa");
         s.setPhoneNo("+234-90564-24816");
 
         System.out.println("Principal: " + s.getPrincipal());
         System.out.println("AccountNo: " + s.getAccountNo());
+        System.out.println("BankName:  " + s.getBankName());
         System.out.println("PhoneNo: " + s.getPhoneNo());
     }
 }
