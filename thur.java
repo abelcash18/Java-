9060801ebb3d1f493ef3262a93799c04d7c0b1be
Joseph Abel <josephabel540@gmail.com>
2026-06-04 10:01:28 +0100

    thur

---
+++ thur.java
@@ -0,0 +1,29 @@
+//thursday class 8am-10am
+//numeric data type -float, double, byte, short, int, long
+// character data type - char
+// boolean data type - true or false
+// primitive data type
+// non primitive data type
+
+
+public class thur {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 20.5f;
+        double c = 30.5;
+        byte d = 100;
+        short e = 20000;
+        long f = 1000000000L;
+        char g = 'A';
+        boolean h = true;
+
+        System.out.println("Integer: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Double: " + c);
+        System.out.println("Byte: " + d);
+        System.out.println("Short: " + e);
+        System.out.println("Long: " + f);
+        System.out.println("Char: " + g);
+        System.out.println("Boolean: " + h);
+    }
+}
